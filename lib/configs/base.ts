b57bafd249b03e4562792f3d7e1019e9846d1ab7
Yosuke Ota <otameshiyo23@gmail.com>
2020-07-14 01:27:44 +0900

    Fixed the parser and the config to accept the extension `.json5`. (#5)

--- lib/configs/base.ts
+++ lib/configs/base.ts
@@ -2,7 +2,7 @@ export = {
     plugins: ["jsonc"],
     overrides: [
         {
-            files: ["*.json"],
+            files: ["*.json", "*.json5"],
             parser: require.resolve("../parser/json-eslint-parser"),
         },
     ],
