483a2d20009837e06e76bbd49b7044ad1ab97160
ota <y.ohta.z3@future.co.jp>
2020-07-14 17:56:34 +0900

    Add `jsonc/no-number-props` rule

---
+++ docs/rules/no-number-props.md
@@ -0,0 +1,46 @@
+---
+pageClass: "rule-details"
+sidebarDepth: 0
+title: "jsonc/no-number-props"
+description: "disallow number property keys"
+---
+# jsonc/no-number-props
+
+> disallow number property keys
+
+- :gear: This rule is included in all of `"plugin:jsonc/recommended-with-json"`, `"plugin:jsonc/recommended-with-json5"` and `"plugin:jsonc/recommended-with-jsonc"`.
+- :wrench: The `--fix` option on the [command line](https://eslint.org/docs/user-guide/command-line-interface#fixing-problems) can automatically fix some of the problems reported by this rule.
+
+## :book: Rule Details
+
+This rule reports the use of number property keys.
+
+JSON, JSONC and JSON5 do not allow number property keys.
+
+<eslint-code-block fix>
+
+```json5
+/* eslint jsonc/no-number-props: 'error' */
+{
+    /* ✓ GOOD */
+    "GOOD": {
+      "42": "foo"
+    },
+
+    /* ✗ BAD */
+    "BAD": {
+      42: "foo"
+    }
+}
+```
+
+</eslint-code-block>
+
+## :wrench: Options
+
+Nothing.
+
+## Implementation
+
+- [Rule source](https://github.com/ota-meshi/eslint-plugin-jsonc/blob/master/lib/rules/no-number-props.ts)
+- [Test source](https://github.com/ota-meshi/eslint-plugin-jsonc/blob/master/tests/lib/rules/no-number-props.js)
---
+++ lib/rules/no-number-props.ts
@@ -0,0 +1,40 @@
+import type { JSONProperty } from "../parser/ast"
+import { createRule } from "../utils"
+
+export default createRule("no-number-props", {
+    meta: {
+        docs: {
+            description: "disallow number property keys",
+            recommended: ["json", "jsonc", "json5"],
+            extensionRule: false,
+        },
+        fixable: "code",
+        schema: [],
+        messages: {
+            unexpected: "The number property keys are not allowed.",
+        },
+        type: "problem",
+    },
+    create(context) {
+        return {
+            JSONProperty(node: JSONProperty) {
+                if (node.key.type !== "JSONLiteral") {
+                    return
+                }
+                if (typeof node.key.value === "number") {
+                    const raw = node.key.raw
+                    context.report({
+                        loc: node.key.loc,
+                        messageId: "unexpected",
+                        fix(fixer) {
+                            return fixer.replaceTextRange(
+                                node.key.range,
+                                `"${raw}"`,
+                            )
+                        },
+                    })
+                }
+            },
+        }
+    },
+})
