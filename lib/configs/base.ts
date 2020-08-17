b964f68659a57568d621464c4ead4103a697c713
Yosuke Ota <otameshiyo23@gmail.com>
2020-08-17 13:43:17 +0900

    Change the base config to turn off ESLint core rules that false positives in JSON. (#21)

--- lib/configs/base.ts
+++ lib/configs/base.ts
@@ -4,6 +4,11 @@ export = {
         {
             files: ["*.json", "*.json5"],
             parser: require.resolve("jsonc-eslint-parser"),
+            rules: {
+                // ESLint core rules known to cause problems with JSON.
+                strict: "off",
+                "no-unused-expressions": "off",
+            },
         },
     ],
 }
