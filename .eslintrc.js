fc2c9d103482b72e4b3a029db7c05a34543485ea
yosuke ota <otameshiyo23@gmail.com>
2021-08-15 10:19:44 +0900

    Add engines section to package.json
    - Change version range of eslint-utils.
    - Upgrade jsonc-eslint-parser.
    - Add engines section to package.json.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
         "plugin:@ota-meshi/+eslint-plugin",
         "plugin:@ota-meshi/+vue2",
         "plugin:@ota-meshi/+json",
+        "plugin:@ota-meshi/+package-json",
         "plugin:@ota-meshi/+yaml",
         "plugin:@ota-meshi/+md",
         "plugin:@ota-meshi/+prettier",
