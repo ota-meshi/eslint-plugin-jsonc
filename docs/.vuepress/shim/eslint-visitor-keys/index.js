fb035f429cc248edf816b663e2d822cc8b7920c8
Yosuke Ota <otameshiyo23@gmail.com>
2023-03-16 14:27:45 +0900

    Update index.js

--- docs/.vuepress/shim/eslint-visitor-keys/index.js
+++ docs/.vuepress/shim/eslint-visitor-keys/index.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair -- DEMO
 /* eslint-disable node/no-unsupported-features/es-syntax -- DEMO */
-import all from "../../../../node_modules/eslint-visitor-keys";
+import all from "../../../../node_modules/eslint-visitor-keys/lib/index.js";
 
 export const { KEYS, getKeys, unionWith } = all;
 export default all;
